2d7526b487fc570ad8d02179b4106dfb985cc1e8
MaitreyPatel <maitreyptl@gmail.com>
2018-02-15 12:53:39 -0500

    added .php file

---
+++ submit.php
@@ -0,0 +1,51 @@
+<html lang="en">
+<?php include "profile/submit.php";?>
+<head>
+<title>Maitrey Patel</title>
+<link rel="shortcut icon" type="img/png" href="img/logo4.PNG">
+<meta charset="utf-8">
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Dancing+Script" />
+<!--<script src="js/skill.js" type="text/JavaScript"></script>-->
+<div class="home"><a name="home"></a>
+
+<div class="menu">
+		<div class="logo">
+			<!--<h2 style="font-family: fantasy; font-size: 30px; color: white;">MP</h2>-->
+      <a href="index.html">
+      <img src="img/logo4.PNG"  height="55" width="55">
+      </a>
+			</div>
+			
+		<ul>
+			
+      <b>
+			<!--<li><a href="#"><h2 class="heading" style="font-family: fantasy; font-size: 18px; color: white; margin-left: 35%;">MP</a></h2></li>-->
+			<li><a href="#home">Home</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="#resume">Resume</a></li>
+			<li><a href="#experience">Experience</a></li>
+			<li><a href="#skills">Skills</a></li>
+			<li><a href="#contact">Contact</a></li>
+			
+      
+      <b>
+		
+		</ul>
+		
+			
+</div>
+
+
+
+	
+</div>
+<h4><center>Form has been submitted</center></h4>
+</head>
+
+</html>
