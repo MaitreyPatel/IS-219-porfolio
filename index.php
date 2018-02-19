f26d8c752224309d7961fb85b2a3122be1603a8f
MaitreyPatel <maitreyptl@gmail.com>
2018-02-19 09:20:55 -0500

    updated .php file 2

--- index.php
+++ index.php
@@ -1,3 +1,270 @@
+
+
+<head>
+<title>Maitrey Patel</title>
+<link rel="shortcut icon" type="img/png" href="img/logo4.PNG">
+<meta charset="utf-8">
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Dancing+Script" />
+<!--<script src="js/skill.js" type="text/JavaScript"></script>-->
+<div class="home"><a name="home"></a>
+<section style="background-image: url('img/background-5.jpg'); width: 100%; height: 820px; background-repeat: no-repeat;">
+
+<div class="menu">
+		<div class="logo">
+			<!--<h2 style="font-family: fantasy; font-size: 30px; color: white;">MP</h2>-->
+      <a href="index.html">
+      <img src="img/logo4.PNG"  height="55" width="55">
+      </a>
+			</div>
+			
+		<ul>
+			
+      <b>
+			<!--<li><a href="#"><h2 class="heading" style="font-family: fantasy; font-size: 18px; color: white; margin-left: 35%;">MP</a></h2></li>-->
+			<li><a href="#home">Home</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="#resume">Resume</a></li>
+			<li><a href="#experience">Experience</a></li>
+			<li><a href="#skills">Skills</a></li>
+			<li><a href="#contact">Contact</a></li>
+			
+      
+      <b>
+		
+		</ul>
+		
+			
+</div>
+
+
+
+	<h4 style="color:white;"><center>I am Maitrey Patel</center></h4>
+	
+	<a href="#contact"><button class="button button1"><b>Hire Me</b></button></a>
+</section>
+</div>
+</head>
+<div id="about"><a name="about"></a>
+<body>
+<br><br>
+<br><br><br><br><br><br><br><br>
+<div class="profile_pic">
+    <img src="img/Capture.JPG" height="300" width="300">
+  </div>
+<div class="about_me">
+	<h1>ABOUT ME</h1><br>
+	 <p>I am Maitrey Patel. I am going to be a recent graduate from New Jersey Insitute of Technology with a Bachelor's of Science in Computer Science and a minor in Web Information's and System(WIS) with a GPA of 3.2. I am very hard working and dedicated toward the work I recieve. I am also very outgoing, I will try my best to achieve the best results as possible. I believe that my experience in Java, Python, SQL, HTML/CSS, PHP, JavaScript and much more will make me a suiteable candidate for your company. My hobbies are to play sports during my free time and to make new websites using HTML/CSS, PHP, JavaScript, and JQuery. I would appreciate the opportunity to meet and further share with you my qualifications and enthusiasm to join the company who is looking for an Entry level candidate.</p>
+</div>
+</div>
+<br><br><br><br><br><br>
+ 
+ <div id="resume"><a name="resume"></a>
+<br><br>
+<br><br><br><br><br><br><br><br>
+
+	<h1><center>Resume<center></h1><br>
+	<div class="resume">
+		<embed src="https://web.njit.edu/~mp458/profile/RESUME.pdf" width="700" height="700" type="application/pdf">
+	</div>
+	
+</div>
+<br><br><br><br><br><br>
+
+ 
+ <div id="experience"><a name="experience"></a>
+<br><br>
+<br><br><br><br><br><br><br><br>
+
+	<h1><center>EXPERIENCE</center></h1><br>
+	<div class="experience">
+  <div class="container right">
+    <div class="content">
+      <h1>Optum, Basking Ridge, NJ</h1>
+      <h2>Software Engineer</h2>
+      <p>â€¢	Helping convert mainframe DB2 COBOL programs to Java programs.</p>
+      <p>â€¢	Making a login page, registration page and a comment system for Archon using PHP, HTML, CSS, JavaScript and SQL.</p>
+      <p>â€¢	Analyzing and debugging code written from COBOL to Java.</p>
+    </div>
+  </div>
+  <div class="container right">
+    <div class="content">
+      <h1>Today's Business, Pine Brook, NJ</h1>
+      <h2>Web/Digital Development</h2>
+      <p>â€¢	Assisting with making updates to the clients website and landing pages from ground up.</p>
+      <p>â€¢	Assist with the project management, content, and development of websites on multiple platforms.</p>
+      <p>â€¢	Design and building Facebook tabs, Landing pages and Mobile Application.</p>
+    </div>
+  </div>
+  
+  <div class="container right">
+    <div class="content">
+      <h1>BJ's Wholesale Club, Jersey City, NJ</h1>
+      <h2>Cashier</h2>
+      <p>â€¢	Help serve customers, and make sure their shopping experience at BJs Wholesale Club is worth remembering so they will come back again and again.</p>
+    </div>
+  </div>
+  
+  <div class="container right">
+    <div class="content">
+      <h1>Jang Star Taekwondo School, Jersey City, NJ</h1>
+      <h2>Balck Belt Instructor</h2>
+      <p>â€¢	Train students to learn Martial Arts so they can achieve their Black Belts.</p>
+    </div>
+  </div>
+</div>
+
+
+</div>
+ <br><br><br><br><br><br>
+ 
+ 
+
+ <div id="skills"><a name="skills"></a>
+ <br><br>
+<br><br><br><br><br><br><br><br>
+	<h1><center>SKILLS</center></h1><br>
+	 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<div class="container-skills">
+					<div class="html">
+					<p class="bar-title">
+						HTML / HTML5 & CSS
+						<span class="percent align-right">90%</span>
+					</p>
+					<div class="bar">
+						<div class="bar-fill bar-fill-html start"></div>
+					</div>
+					</div>
+					<div class="javascript">
+						<p class="bar-title">
+							Javascript
+							<span class="percent align-right">50%</span>
+						</p>
+						<div class="bar">
+							<div class="bar-fill bar-fill-javascript start"></div>
+						</div>
+					</div>
+					<div class="jquery">
+						<p class="bar-title">
+							JQuery
+							<span class="percent align-right">50%</span>
+						</p>
+						<div class="bar">
+							<div class="bar-fill bar-fill-jquery start"></div>
+						</div>
+					</div>
+					<div class="php">
+						<p class="bar-title">
+							PHP
+							<span class="percent align-right">70%</span>
+						</p>
+						<div class="bar">
+							<div class="bar-fill bar-fill-php start"></div>
+						</div>
+					</div>
+					<div class="java">
+						<p class="bar-title">
+							Java
+							<span class="percent align-right">80%</span>
+						</p>
+						<div class="bar">
+						<div class="bar-fill bar-fill-java start"></div>
+						</div>
+					</div>
+          <div class="python">
+						<p class="bar-title">
+							Python
+							<span class="percent align-right">90%</span>
+						</p>
+						<div class="bar">
+						<div class="bar-fill bar-fill-python start"></div>
+						</div>
+					</div> 
+          <div class="sql">
+						<p class="bar-title">
+							SQL
+							<span class="percent align-right">60%</span>
+						</p>
+						<div class="bar">
+						<div class="bar-fill bar-fill-sql start"></div>
+						</div>
+					</div>
+          <div class="bootstrap">
+						<p class="bar-title">
+							Bootstrap
+							<span class="percent align-right">80%</span>
+						</p>
+						<div class="bar">
+						<div class="bar-fill bar-fill-bootstrap start"></div>
+						</div>
+					</div>
+          <div class="linux">
+						<p class="bar-title">
+							Linux
+							<span class="percent align-right">60%</span>
+						</p>
+						<div class="bar">
+						<div class="bar-fill bar-fill-linux start"></div>
+						</div>
+					</div>
+          <div class="software_development">
+						<p class="bar-title">
+							Software Development 
+							<span class="percent align-right">80%</span>
+						</p>
+						<div class="bar">
+						<div class="bar-fill bar-fill-software_development start"></div>
+						</div>
+					</div>     
+				</div>	
+</div>
+ <br><br><br><br><br><br>
+
+<div id="contact"><a name="contact"></a>
+<br><br>
+<br><br><br><br><br><br><br><br>
+
+	<h1><center>FEEL FREE TO CONTACT ME<center></h1><br>
+
+	<div class="contact_info">
+	<div id="email">
+		<i class="fa fa-envelope" style="font-size:48px; padding-left:0.3%;"></i>
+		<p>Email:</p>
+		<p>mp458@njit.edu</p>
+	</div>
+	<div id="address">
+		<i class="fa fa-building" style="font-size:48px; padding-left:0.3%;"></i>
+		<p>Address:</p>
+		<p>407 Liberty Ave. Fl#1</p>
+		<p>Jersey City, NJ, 07307</p>
+	</div>
+	<div id="phone">
+		<i class="fa fa-phone" style="font-size:48px; padding-left:0.3%;"></i>
+		<p>Phone Number:</p>
+		<p>551-689-3157</p>
+	</div>
+	
+	</div>
+</div>
+
+ <br><br><br><br><br><br>
+ 
+</body>
+<br><br><br><br><br><br><br><br><br>
+<div class="form">
+	<form style="float:center;" action="/action_page.php">
+		<input style="margin-bottom: 5%; margin-left: 5%;" placeholder="Name" type="text" name="Name">
+		<input style="margin-bottom: 5%; margin-left: 5%;" type="text" name="Email" placeholder="Email"><br>
+		<textarea style="margin-left: 5%; margin-bottom: 5%;" placeholder="Type Here" rows="5" cols="60"></textarea><br>
+		<a href="submit.php"><input style= "margin-left: 20%;" type="button" value="Send" class="button1"></a>
+	</form>
+</div>
+</html>
 <html lang="en">
 
 <head>
